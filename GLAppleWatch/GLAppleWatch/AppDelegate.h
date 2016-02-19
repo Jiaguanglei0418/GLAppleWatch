c0c9b6173860f6f6d91818f6e79cc63a55420a28
jiaguanglei <2318336342@qq.com>
2016-02-19 14:34:48 +0800

    init

---
+++ GLAppleWatch/GLAppleWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLAppleWatch
+//
+//  Created by jiaguanglei on 16/2/19.
+//  Copyright © 2016年 roseonly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
